93f9c0b6f793efb08ee6374af3933d27f3ec7d8d
ecalzo <evancalz@me.com>
2021-07-18 17:45:22 -0400

    feat: serializer for rendering database data as JSON

--- news_ai_choose/models.py
+++ news_ai_choose/models.py
@@ -1,9 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from sqlalchemy.inspection import inspect
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class News(db.Model):
+
+class Serializer(object):
+
+    def serialize(self):
+        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+
+    @staticmethod
+    def serialize_list(l):
+        return [m.serialize() for m in l]
+
+
+class News(db.Model, Serializer):
     __tablename__ = 'news'
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     title = db.Column(db.Text, nullable=False)
@@ -13,5 +26,11 @@ class News(db.Model):
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
     updated_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
 
+    def serialize(self):
+        serial = Serializer.serialize(self)
+        serial["created_at"] = serial["created_at"].strftime("%Y-%m-%d %H:%M%:%S")
+        serial["updated_at"] = serial["updated_at"].strftime("%Y-%m-%d %H:%M%:%S")
+        return serial
+    
     def __repr__(self):
         return '<News %r>' % self.title
